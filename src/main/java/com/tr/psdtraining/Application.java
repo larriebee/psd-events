e0948e11d44669b91fe28df439251bb648550639
Ben Fowler <ben.fowler.bjf@gmail.com>
2016-06-21 12:27:49 +0100

    Initial checkin

---
+++ src/main/java/com/tr/psdtraining/Application.java
@@ -0,0 +1,3 @@
+package com.tr.psdtraining;
+
+public interface Application {}
---
+++ src/main/java/com/tr/psdtraining/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.tr.psdtraining.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import com.tr.psdtraining.Application;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
